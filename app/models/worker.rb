69e760b6faed1e0c2d72a755a2599700b9f3ca17
Marty Kovach <tyedye105@yahoo.com>
2017-02-24 10:40:40 -0800

    create method for marking jobs active. passing specs

--- app/models/worker.rb
+++ app/models/worker.rb
@@ -4,4 +4,13 @@ class Worker < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :jobs
+  def mark_active(job)
+    job_to_activate = self.jobs.find(job.id)
+    if self.id === job_to_activate.worker_id
+      job_to_activate.is_active = true
+    else
+      return false
+    end
+    return job_to_activate
+  end
 end
